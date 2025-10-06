bbebb4315d86f4a2813a77e0788eacc919d13ea4
Sandali Karunarathna <sandalikaru123@gmail.com>
2025-10-06 05:34:19 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: { unoptimized: true },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
